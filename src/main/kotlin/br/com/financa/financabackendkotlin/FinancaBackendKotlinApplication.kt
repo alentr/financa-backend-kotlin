48446ebd98e5d4dd483f11a149038c5cc61ba545
Alexandre Telles <alexandre.ntr@gmail.com>
2022-06-06 09:39:20 +0100

    Initial commit

---
+++ src/main/kotlin/br/com/financa/financabackendkotlin/FinancaBackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.financa.financabackendkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancaBackendKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancaBackendKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/financa/financabackendkotlin/FinancaBackendKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.financa.financabackendkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancaBackendKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
