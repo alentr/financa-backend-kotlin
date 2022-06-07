d6b4ca5b7ef08a0b63e9a5aca47e2403dced6ae1
Alexandre Telles <alexandre.ntr@gmail.com>
2022-06-07 16:04:40 +0100

    migrating from Java to Kotlin

--- src/main/kotlin/br/com/financa/categoriatransacao/adapter/input/controllers/CategoriaTransacaoController.kt
+++ src/main/kotlin/br/com/financa/categoriatransacao/adapter/input/controllers/CategoriaTransacaoController.kt
@@ -1,33 +1,26 @@
 package br.com.financa.categoriatransacao.adapter.input.controllers
 
-import br.com.financa.Application
 import br.com.financa.categoriatransacao.adapter.input.controllers.dtos.OutputCategoriaTransacaoDTO
 import br.com.financa.categoriatransacao.application.ports.`in`.ICategoriaTransacaoPortIn
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Validated
-@RequestMapping("/financa}")
+@RequestMapping("/gerenciamento/categoria-transacao")
 class CategoriaTransacaoController @Autowired constructor(private val categoriaTransacaoPortIn: ICategoriaTransacaoPortIn) {
 
-    @RequestMapping(
-        method = [RequestMethod.GET],
-        value = ["/gerenciamento/categoria-transacao"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun consultarCategoriasTransacaoPorNome(
-        usuario: String, nome: String?
+        @RequestHeader(value = "usuario", required = true) usuario: String,
+        @RequestParam(value = "nome", required = false) nome: String?
     ): ResponseEntity<List<OutputCategoriaTransacaoDTO>> {
 
 
-        val consultarCategoriasTransacaoPorNome =
+        val response =
             categoriaTransacaoPortIn.consultarCategoriasTransacaoPorNome(usuario, nome)
 
         return ResponseEntity.ok(listOf())
